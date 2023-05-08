7a71e89276eb6af99be306911f9c763956814c01
sabry82 <sabry82@gmail.com>
2023-05-08 18:17:40 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
 import Pagination from './components/Pagination';
 
 function App() {
 
   const onPageChange = (page) => {
     console.log("componente padre: ", page)
+    setPage(page);
   }
+
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [pokemons, setPokemons] = useState([]);
+
+  const getEndpoint = (pagina) => {
+    return `https://pokeapi.co/api/v2/ability/?limit=20&offset=${pagina}`;
+  }
+
+  useEffect(() => {
+    fetch(getEndpoint(page))
+      .then(response => response.json())
+      .then(data => {
+        setTotal(data.count);
+        setPokemons(data.results);
+      })
+  }, [page]);
 
   return (
     <div className="App">
-      <Pagination total={65} page={3} itemsPerPage={10} onChange={onPageChange}/>
+      {total}
+      {pokemons && pokemons.map((p, i) => (
+        <p key={i}>{p.name}</p>
+      ))}
+
+      <Pagination total={total} page={page} itemsPerPage={20} onChange={onPageChange} />
     </div>
   );
 }
