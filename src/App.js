ed36352f92b477000488eeaad855e08f5d3661c7
Daniela <danscarballo@gmail.com>
2023-05-10 17:26:50 -0300

    Paginado

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Pagination from './components/Pagination';
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [id, setId] = useState(1);
+  const [pokemon, setPokemon] = useState({});
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then(response => response.json())
+      .then(data => setPokemon(data));
+  }, [id])
+
   const onPageChange = (page) => {
-    console.log("componente padre: ", page)
+    setId(page);
+    console.log(pokemon);
   }
 
   return (
     <div className="App">
-      <Pagination total={65} page={3} itemsPerPage={10} onChange={onPageChange}/>
+      {pokemon.name ? <h1>{pokemon.name}</h1> : null}
+
+      {pokemon.sprites ? (
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} key={pokemon.id} />
+      ) : null}
+
+      <Pagination
+        total={150}
+        page={id}
+        itemsPerPage={3}
+        onChange={onPageChange}
+      />
     </div>
   );
 }
 
 export default App;
+
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,50 +1,67 @@
-import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 
 const Pagination = ({ page, total, itemsPerPage, onChange }) => {
-    const [currentPage, setCurrentPage] = useState(page);
+  const [currentPage, setCurrentPage] = useState(page);
 
-    const totalPages = Math.ceil(total / itemsPerPage);
+  const totalPages = Math.ceil(total / itemsPerPage);
 
-    const pagesArray = [];
+  const pagesArray = [];
 
-    const handleChangePage = (e) => {
-        const newPage = Number(e.target.value);
-        setCurrentPage(newPage);
+  for (let i = 1; i <= totalPages; i++) {
+    pagesArray.push(i);
+  }
 
-        if (onChange)
-            onChange(newPage);
-    }
+  const handleChangePage = (e) => {
+    const newPage = Number(e.target.value);
+    setCurrentPage(newPage);
 
-    for (let index = 1; index <= totalPages; index++) {
-        pagesArray.push(index);
-    }
+    if (onChange) onChange(newPage);
+  };
 
-    return (
-        <div>
-            <Button onClick={handleChangePage}
-                value={1}
-                disabled={currentPage === 1}>Primera</Button>{' '}
-            <Button onClick={handleChangePage}
-                value={currentPage - 1}
-                disabled={currentPage === 1}> {"<"} </Button>{' '}
-            {pagesArray.map((pageNumber, i) => (
-                <>
-                    <Button key={i}
-                        onClick={handleChangePage}
-                        value={pageNumber}
-                        variant='primary'
-                        disabled={pageNumber === currentPage}>{pageNumber}</Button>{' '}
-                </>
-            ))}
-            <Button onClick={handleChangePage}
-                value={currentPage + 1}
-                disabled={currentPage === totalPages}> {">"} </Button>{' '}
-            <Button onClick={handleChangePage}
-                value={totalPages}
-                disabled={currentPage === totalPages}>Ultima</Button>
-        </div>
-    );
+  return (
+    <div>
+      <Button onClick={handleChangePage} value={1} disabled={currentPage === 1}>
+        Primera
+      </Button>{" "}
+      <Button
+        onClick={handleChangePage}
+        value={currentPage - 1}
+        disabled={currentPage === 1}
+      >
+        {" "}
+        {"<"}{" "}
+      </Button>{" "}
+      {pagesArray.map((pageNumber, i) => (
+        <>
+          <Button
+            key={i}
+            onClick={handleChangePage}
+            value={pageNumber}
+            variant="primary"
+            disabled={pageNumber === currentPage}
+          >
+            {pageNumber}
+          </Button>{" "}
+        </>
+      ))}
+      <Button
+        onClick={handleChangePage}
+        value={currentPage + 1}
+        disabled={currentPage === totalPages}
+      >
+        {" "}
+        {">"}{" "}
+      </Button>{" "}
+      <Button
+        onClick={handleChangePage}
+        value={totalPages}
+        disabled={currentPage === totalPages}
+      >
+        Ultima
+      </Button>
+    </div>
+  );
 };
 
 export default Pagination;
